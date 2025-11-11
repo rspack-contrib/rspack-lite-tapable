fa60da54ad1bdb7dee95643c21a9fb5143b84e44
neverland <chenjiahan.jait@bytedance.com>
2025-11-11 14:34:29 +0800

    chore: simplify Rslib build config

--- rslib.config.ts
+++ rslib.config.ts
@@ -3,12 +3,9 @@ import { defineConfig } from '@rslib/core';
 export default defineConfig({
   lib: [
     { format: 'esm', syntax: 'es2021', dts: true },
-    { format: 'cjs', syntax: 'es2021', dts: true },
+    { format: 'cjs', syntax: 'es2021' },
   ],
   source: {
     tsconfigPath: './tsconfig.build.json',
-  },
-  output: {
-    sourceMap: false,
   },
 });
